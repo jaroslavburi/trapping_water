c5fa7d3392c0e48807ed704457f8e9208bc4f644
Jaroslav B <jaroslavburi@gmail.com>
2018-10-21 16:15:54 +0200

    found bug and rewrote using two pointers

--- bar_chart_water.py
+++ bar_chart_water.py
@@ -1,38 +1,37 @@
 #!/usr/bin/python
-
-def bar_chart_water(array):
-    #helper variables
-    max = 0
-    max_pos = -1
-    second_highest = 0
-    second_highest_pos = 0
-
-    last_element_idx = len(array) -1;
+class bar_chart_water:
 
     water_amount = 0
-    for idx, bar in enumerate(array):
-        if bar >= max:
-            max = bar
-            max_pos = idx
-            second_highest = 0
-            second_highest_pos = 0
+    array = None
+    
+    def walk_array(self, idx, max):
 
-        else: #drop -> start counting
-            water_amount += max - bar
+        if self.array[idx] > max:
+            max = self.array[idx]
+        else: #drop
+            self.water_amount += max - self.array[idx]
 
-            if bar >= second_highest:
-                second_highest = bar
-                second_highest_pos = idx
+        return idx, max
 
-            #last element and we didnt find same high border - need to substract missing water
-            if idx == last_element_idx:
-                width = last_element_idx - max_pos
-                height = max - second_highest
-                water_amount -= width * height
+    def run(self):
+        left_idx = 0
+        right_idx = len(self.array) -1;
 
-                #remove water on the far side without border
-                width = last_element_idx - second_highest_pos
-                height = max - second_highest
-                water_amount -= width * height
+        left_max = 0
+        right_max = 0
 
-    return water_amount
+        while left_idx <= right_idx:
+
+            if (left_max <= right_max):
+                left_idx, left_max = self.walk_array(left_idx, left_max)
+                left_idx+=1
+
+            else: #go from right
+                right_idx, right_max = self.walk_array(right_idx, right_max)
+                right_idx-=1
+
+        return self.water_amount
+
+    def __init__(self, array):
+        self.array = array
+        
--- test.py
+++ test.py
@@ -5,40 +5,48 @@ import unittest
 class TestBarChart(unittest.TestCase):
 
     def test_standard(self):
-        a = [1,2,3,4,5,1,2,3,4,1,2,3,4,5,1,2,3,4,3,3,3]
-        self.assertEqual(bar_chart_water(a), 26)
+        b = bar_chart_water([1,2,3,4,5,1,2,3,4,1,2,3,4,5,1,2,3,4,3,3,3])
+        self.assertEqual(b.run(), 26)
+
+    def test_bug(self):
+        b = bar_chart_water([5,4,3,4,3,2,1,0])
+        self.assertEqual(b.run(), 1)
+
+    def test_bug2(self):
+        b = bar_chart_water([3,2,1,2,1,0,1,0])
+        self.assertEqual(b.run(), 2)
 
     def test_internet_example(self):
-        a = [0,1,0,2,1,0,1,3,2,1,2,1]
-        self.assertEqual(bar_chart_water(a), 6)
+        b = bar_chart_water([0,1,0,2,1,0,1,3,2,1,2,1])
+        self.assertEqual(b.run(), 6)
 
     def test_zero(self):
-        a = [0,0,0]
-        self.assertEqual(bar_chart_water(a), 0)
+        b = bar_chart_water([0,0,0])
+        self.assertEqual(b.run(), 0)
 
     def test_edge_case1(self):
-        a = [0,1,0]
-        self.assertEqual(bar_chart_water(a), 0)
+        b = bar_chart_water([0,1,0])
+        self.assertEqual(b.run(), 0)
     
     def test_edge_case2(self):
-        a = [1,0,0]
-        self.assertEqual(bar_chart_water(a), 0)
+        b = bar_chart_water([1,0,0])
+        self.assertEqual(b.run(), 0)
     
     def test_edge_case3(self):
-        a = [0,0,1]
-        self.assertEqual(bar_chart_water(a), 0)
+        b = bar_chart_water([0,0,1])
+        self.assertEqual(b.run(), 0)
 
     def test_edge_case4(self):
-        a = [1,0,1]
-        self.assertEqual(bar_chart_water(a), 1)
+        b = bar_chart_water([1,0,1])
+        self.assertEqual(b.run(), 1)
 
     def test_edge_case5(self):
-        a = [1,0,1,0,1]
-        self.assertEqual(bar_chart_water(a), 2)
+        b = bar_chart_water([1,0,1,0,1])
+        self.assertEqual(b.run(), 2)
 
     def test_edge_case6(self):
-        a = [1,1,1]
-        self.assertEqual(bar_chart_water(a), 0)
+        b = bar_chart_water([1,1,1])
+        self.assertEqual(b.run(), 0)
 
 if __name__ == '__main__':
     unittest.main()
